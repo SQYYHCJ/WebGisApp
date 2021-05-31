31191c0a20fe04711eaa93a19bae3d734e054a73
HCJ <331830772@qq.com>
2021-05-31 23:21:46 +0800

    实验7完成 绘制线，绘制虚线 绘制彩色线，线点击事件

--- app/src/main/java/top/sqyy/webgis/MainActivity.java
+++ app/src/main/java/top/sqyy/webgis/MainActivity.java
@@ -3,9 +3,11 @@ package top.sqyy.webgis;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.BDLocation;
@@ -22,6 +24,7 @@ import com.baidu.mapapi.map.MarkerOptions;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.map.Overlay;
 import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.map.Polyline;
 import com.baidu.mapapi.map.PolylineOptions;
 import com.baidu.mapapi.map.UiSettings;
 import com.baidu.mapapi.model.LatLng;
@@ -137,6 +140,75 @@ public class MainActivity extends AppCompatActivity {
             //在地图上绘制折线
             //mPloyline 折线对象
             Overlay mPolyline = mBaiduMap.addOverlay(mOverlayOptions);
+//            ((Polyline)mPolyline).setDottedLine(true);
+            return true;
+        }
+
+        if (id == R.id.drawDottedLine) {
+            mBaiduMap.clear();
+            //构建折线点坐标,
+            LatLng p1 = new LatLng(60.527123, 80.405671);
+            LatLng p2 = new LatLng(60.526779, 160.405241);
+            LatLng p3 = new LatLng(40.526149, 100.40724);
+            LatLng p4 = new LatLng(80.527123, 120.405671);
+            LatLng p5 = new LatLng(40.526779, 140.405241);
+            LatLng p6 = new LatLng(60.526149, 80.40724);
+            List<LatLng> points = new ArrayList<LatLng>();
+            points.add(p1);
+            points.add(p2);
+            points.add(p3);
+            points.add(p4);
+            points.add(p5);
+            points.add(p6);
+
+            //设置折线的属性
+            OverlayOptions mOverlayOptions = new PolylineOptions()
+                    .width(10)
+                    .color(0xAAFF0000)
+                    .points(points)
+                    .dottedLine(true); //设置折线显示为虚线
+            //mPloyline 折线对象
+            Overlay mPolyline = mBaiduMap.addOverlay(mOverlayOptions);
+            //设置折线显示为虚线
+            ((Polyline) mPolyline).setDottedLine(true);
+            return true;
+        }
+
+        if (id == R.id.drawColorLine) {
+            mBaiduMap.clear();
+            //构建折线点坐标
+            List<LatLng> points = new ArrayList<LatLng>();
+            LatLng p1 = new LatLng(60.527123, 80.405671);
+            LatLng p2 = new LatLng(60.526779, 160.405241);
+            LatLng p3 = new LatLng(40.526149, 100.40724);
+            LatLng p4 = new LatLng(80.527123, 120.405671);
+            LatLng p5 = new LatLng(40.526779, 140.405241);
+            LatLng p6 = new LatLng(60.526149, 80.40724);
+            points.add(p1);
+            points.add(p2);
+            points.add(p3);
+            points.add(p4);
+            points.add(p5);
+            points.add(p6);
+
+            List<Integer> colors = new ArrayList<>();
+            colors.add(Integer.valueOf(Color.BLUE));
+            colors.add(Integer.valueOf(Color.RED));
+            colors.add(Integer.valueOf(Color.YELLOW));
+            colors.add(Integer.valueOf(Color.GREEN));
+            colors.add(Integer.valueOf(Color.BLUE));
+            colors.add(Integer.valueOf(Color.RED));
+
+//设置折线的属性
+            OverlayOptions mOverlayOptions = new PolylineOptions()
+                    .width(10)
+                    .color(0xAAFF0000)
+                    .points(points)
+                    .colorsValues(colors);//设置每段折线的颜色
+
+//在地图上绘制折线
+//mPloyline 折线对象
+            Overlay mPolyline = mBaiduMap.addOverlay(mOverlayOptions);
             return true;
         }
 
@@ -172,6 +244,22 @@ public class MainActivity extends AppCompatActivity {
             mBaiduMap.addOverlay(option);
             return true;
         }
+
+        if (id == R.id.drawClickLine) {
+            BaiduMap.OnPolylineClickListener listener = new BaiduMap.OnPolylineClickListener() {
+                //处理Polyline点击逻辑
+                @Override
+                public boolean onPolylineClick(Polyline polyline) {
+                    Toast.makeText(MainActivity.this, "Click on polyline", Toast.LENGTH_LONG).show();
+
+                    return true;
+                }
+            };
+            //设置Polyline点击监听器
+            mBaiduMap.setOnPolylineClickListener(listener);
+            return true;
+        }
+
 
         return super.onOptionsItemSelected(item);
     }
