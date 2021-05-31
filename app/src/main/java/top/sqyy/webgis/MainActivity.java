d8c3daffc7a08c5258aefa4376ad58a04e0d5a43
HCJ <331830772@qq.com>
2021-05-31 16:30:58 +0800

    实验二完成

--- app/src/main/java/top/sqyy/webgis/MainActivity.java
+++ app/src/main/java/top/sqyy/webgis/MainActivity.java
@@ -4,11 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.baidu.mapapi.CoordType;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
 public class MainActivity extends AppCompatActivity {
+    private MapView mMapView =null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SDKInitializer.initialize(getApplicationContext());
+
+        SDKInitializer.setCoordType(CoordType.BD09LL);
+
         setContentView(R.layout.activity_main);
+
+        mMapView = findViewById(R.id.bmapView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
     }
 }
